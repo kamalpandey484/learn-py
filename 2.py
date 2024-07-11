2d7c7c2b33187b8dd1df023049a75a4362a05784
kamalpandey484 <kamalpandey484@gmail.com>
2024-07-11 18:22:17 +0000

    Comments and prints

---
+++ 2.py
@@ -0,0 +1,26 @@
+#print string with integer
+print("Hello world", 5)
+
+#printing the sum of two numbers
+print(10+20)
+
+#print value in multiple lines
+print("Line one\nLine two\nLine three")
+
+'''
+Multi 
+line
+comment
+'''
+
+"""
+Multi 
+line
+comment
+"""
+
+print("Hey", 10+20, "Hello", sep="~")
+
+print("Hey", 10+20, "Hello", sep="~", end="test\n")
+
+print("New line")
