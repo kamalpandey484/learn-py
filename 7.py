18fb6213fb5ac668ed7f312980f2428574c285cb
kamalpandey484 <kamalpandey484@gmail.com>
2024-07-12 05:13:34 +0000

    strings

---
+++ 7.py
@@ -0,0 +1,17 @@
+#Strings
+string1 = "string"
+string2 = "string 2"
+
+print(string1[1])
+
+#multiline string
+string3 = """
+This is a string
+with multiple lines
+"""
+
+print("Multiline string:", string3)
+
+for char in string1:
+  print(char)
+  
