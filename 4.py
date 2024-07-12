a03cde18b99b6a84ea82f82bca6dcb4c78b642e1
kamalpandey484 <kamalpandey484@gmail.com>
2024-07-12 04:26:49 +0000

    Arithmatic operator

---
+++ 4.py
@@ -0,0 +1,29 @@
+#Operators
+
+#arithmatic operators
+num1 = 10
+num2 = 5
+
+#Addition
+sum = num1 + num2
+print(sum)
+
+#Subtraction
+sub = num1 - num2
+print(sub)
+
+#Multiplication
+mul = num1 * num2
+print(mul)
+
+#Division
+div = num1 / num2
+print(div)
+
+#Floor Division
+floorDiv = num1 // num2
+print(floorDiv)
+
+#Modulus
+mod = num1 % num2
+print(mod)
